45ebdaedba5e2d52742793ba1111fbe213c303f2
Barak Yoresh <barak@lightricks.com>
2015-09-20 16:57:33 +0300

    Initial Commit

---
+++ ToneMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToneMapper
+//
+//  Created by Barak Yoresh on 9/20/15.
+//  Copyright (c) 2015 Barak Yoresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToneMapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToneMapper
+//
+//  Created by Barak Yoresh on 9/20/15.
+//  Copyright (c) 2015 Barak Yoresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
